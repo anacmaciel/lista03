be329ffb08d86c3102f5defeafee3abb0f20c9b9
anacmaciel <casmaciel96@gmail.com>
2022-06-24 19:38:38 -0300

    subindo resolução exercício 1

---
+++ src/decisao/exercicio1/maiorNumero.java
@@ -0,0 +1,25 @@
+package decisao.exercicio1;
+
+import java.util.Scanner;
+
+public class maiorNumero {
+
+    public static void main(String[] args) {
+
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o primeiro número");
+        int primeiroNumero = entrada.nextInt();
+        System.out.println("Digite o segundo número");
+        int segundoNumero = entrada.nextInt();
+        entrada.close();
+
+        if (primeiroNumero > segundoNumero) {
+            System.out.println("O maior número foi " + primeiroNumero);
+        } else if (segundoNumero == primeiroNumero) {
+            System.out.println("O primeiro número foi igual ao segundo. ");
+        }
+        if (segundoNumero > primeiroNumero)
+            System.out.println("O maior número foi" + segundoNumero);
+    }
+}
