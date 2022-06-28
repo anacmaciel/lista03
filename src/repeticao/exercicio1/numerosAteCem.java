ba56992321313a01975ca8601f7cf7383178bd9e
anacmaciel <casmaciel96@gmail.com>
2022-06-28 17:21:45 -0300

    subindo resolução do exercício 1 lista 003 - b

---
+++ src/repeticao/exercicio1/numerosAteCem.java
@@ -0,0 +1,20 @@
+package repeticao.exercicio1;
+
+public class numerosAteCem {
+
+    public static void main(String[] args) {
+
+
+    for(int i = 1; i <= 100; i++) {
+        System.out.println(i);
+    }
+
+        int j = 1;
+        while (j <= 100) {
+            System.out.println(j);
+            j++;
+        }
+
+
+    }
+}
